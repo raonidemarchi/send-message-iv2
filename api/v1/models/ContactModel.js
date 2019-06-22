637a5a97c8d96ad3495fb2027b5462f01cccc532
Raoni Demarchi <raonidemarchi@gmail.com>
2019-06-22 15:45:50 -0300

    change name from the column to dateCreated

--- api/v1/models/ContactModel.js
+++ api/v1/models/ContactModel.js
@@ -8,7 +8,7 @@ function ContactModel() {
 		company: { type: String, required: true },
 		givewayParticipant: { type: Boolean, required: true },
     giveawayNumber: { type: Number, required: false, default: null },
-    dateCreated: { type: Number, required: false, default: new Date().getTime() },
+    createdDate: { type: Number, required: false, default: new Date().getTime() },
 	};
 
 	const newSchema = new Schema(_schema, { versionKey: false });
